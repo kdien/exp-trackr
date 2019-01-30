2d47d73f8d01ac7dd1cfaae93f702d774b299395
WinterAnimosus <WinterAnimosus_nathan@hotmail.com>
2019-01-30 14:28:34 -0400

    Add models

---
+++ ExpTrackr/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ExpTrackr.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Expense> Expenses { get; set; }
+    }
+}
---
+++ ExpTrackr/Models/Expense.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ExpTrackr.Models
+{
+    public class Expense
+    {
+        public int ExpenseID { get; set; }
+
+        public virtual Budget Budget { get; set; }
+        public int BudgetID { get; set; }
+
+        public virtual Category Category { get; set; }
+        public int CategoryID { get; set; }
+
+        [Display(Name = "Description")]
+        public string ExpenseDescription { get; set; }
+
+        [Display(Name = "Amount")]
+        [DataType(DataType.Currency)]
+        public decimal ExpenseAmount { get; set; }
+    }
+}
