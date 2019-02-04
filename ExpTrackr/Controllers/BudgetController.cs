713c94c2a564de2c5f43cee04d4b896bb3473569
WinterAnimosus <WinterAnimosus_nathan@hotmail.com>
2019-02-04 14:20:17 -0400

    Colors, nav, bit of css, login / hidden page css, scaffolded controller

---
+++ ExpTrackr/Controllers/BudgetController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExpTrackr.Data;
+using ExpTrackr.Models;
+
+namespace ExpTrackr.Controllers
+{
+    public class BudgetController : Controller
+    {
+        private readonly ExpTrackrContext _context;
+
+        public BudgetController(ExpTrackrContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Budget
+        public async Task<IActionResult> Index()
+        {
+            var expTrackrContext = _context.Budgets.Include(b => b.User);
+            return View(await expTrackrContext.ToListAsync());
+        }
+
+        // GET: Budget/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var budget = await _context.Budgets
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.BudgetID == id);
+            if (budget == null)
+            {
+                return NotFound();
+            }
+
+            return View(budget);
+        }
+
+        // GET: Budget/Create
+        public IActionResult Create()
+        {
+            ViewData["UserID"] = new SelectList(_context.Users, "UserID", "Email");
+            return View();
+        }
+
+        // POST: Budget/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BudgetID,UserID,BudgetName,BudgetMax,BudgetTotal,CreationDate")] Budget budget)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(budget);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "UserID", "Email", budget.UserID);
+            return View(budget);
+        }
+
+        // GET: Budget/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var budget = await _context.Budgets.FindAsync(id);
+            if (budget == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "UserID", "Email", budget.UserID);
+            return View(budget);
+        }
+
+        // POST: Budget/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BudgetID,UserID,BudgetName,BudgetMax,BudgetTotal,CreationDate")] Budget budget)
+        {
+            if (id != budget.BudgetID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(budget);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BudgetExists(budget.BudgetID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "UserID", "Email", budget.UserID);
+            return View(budget);
+        }
+
+        // GET: Budget/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var budget = await _context.Budgets
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.BudgetID == id);
+            if (budget == null)
+            {
+                return NotFound();
+            }
+
+            return View(budget);
+        }
+
+        // POST: Budget/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var budget = await _context.Budgets.FindAsync(id);
+            _context.Budgets.Remove(budget);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BudgetExists(int id)
+        {
+            return _context.Budgets.Any(e => e.BudgetID == id);
+        }
+    }
+}
