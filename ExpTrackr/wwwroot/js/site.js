f9001514ceb621c53ee28fd57c0af5c7154cec9d
Khoa Dien <khoa.dien@hotmail.com>
2019-02-10 00:54:00 -0400

    Modify front end of all Category views and Budget Index view

--- ExpTrackr/wwwroot/js/site.js
+++ ExpTrackr/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(function () {
+    // Make the whole budget row clickable
+    $("#BudgetTable tbody tr")
+        .click(function () {
+            window.location = $(this).find("a[href*='Expense']").attr("href");
+        })
+        .hover(function () {
+            $(this).toggleClass("hover");
+        });
+});
