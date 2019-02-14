1042a48789d112ebf74f21e0d1ec8b3b99e1d70a
Khoa Dien <khoa.dien@hotmail.com>
2019-02-13 21:22:12 -0400

    Add jQuery code to show and hide form, also to make form show by default if there is error

--- ExpTrackr/wwwroot/js/site.js
+++ ExpTrackr/wwwroot/js/site.js
@@ -8,9 +8,20 @@
             $(this).toggleClass("hover");
         });
 
+    // Click plus button to show and hide form
     $("#CreateCategoryButton").click(function () {
         $(this).find("i").toggleClass("rotate");
         $(this).toggleClass("button-click");
         $("#CreateCategoryForm").toggle(400);
     });
+
+    // If form has error then show it by default
+    if ($("form span[class*='field-validation']").html().length > 0) {
+
+        $("form").show();
+
+        $("html, body").animate({
+            scrollTop: $("form span[class*='field-validation']").offset().top
+        });
+    }
 });
